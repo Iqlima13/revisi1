0c3cb649656896ad295d25ad18a5ce33421e6d9e
Iqlima13 <140933247+Iqlima13@users.noreply.github.com>
2023-08-26 13:39:48 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,49 @@
+<html>
+
+<head>
+  <title>PKL BPBD</title>
+  <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="assets/style.css">
+</head>
+
+<body>
+  <div id="container">
+
+    <nav>
+      <div id="logo">
+        <img src="logobpbd4.png" alt="logo bpbd">
+        <span class="textbrand">BPBD</span>
+      </div>
+      <ul>
+        <li><a href="index.php">Beranda</a></li>
+        <li class="dropdown" onmouseover="hover(this);" onmouseout="out(this);"><a href="#">Profil &nbsp;<i class="fa fa-caret-down"></i></a>
+          <div class="dd">
+            <div id="up_arrow"></div>
+            <ul>
+
+              <li><a href="sejarah.php">Sejarah</a></li>
+              <li><a href="visimisi.php">Visi Misi</a></li>
+              <li><a href="struktur.php">Struktur Organisasi</a></li>
+            </ul>
+          </div>
+        </li>
+        <li><a href="peraturan.php">Peraturan</a></li>
+        <li><a href="info.php">Info Kendaraan</a></li>
+        <li><a href="pengaduan.php">Pengaduan</a></li>
+      </ul>
+  </div>
+  </ul>
+  </nav>
+  <img src="bpbd.jpg" alt="Gambar_BPBD" style="width:100%;height:510px;">
+  <h5> Badan Penanggulangan Bencana Daerah (BPBD) adalah lembaga pemerintah non-departemen yang melaksanakan
+    tugas penanggulangan bencana di daerah baik Provinsi maupun Kabupaten/ Kota dengan berpedoman pada
+    kebijakan yang ditetapkan oleh Badan Nasional Penanggulangan Bencana. BPBD dibentuk berdasarkan Peraturan
+    Presiden Nomor 8 Tahun 2008, menggantikan Satuan Koordinasi Pelaksana Penanganan Bencana (Satkorlak) di tingkat
+    Provinsi dan Satuan Pelaksana Penanganan Bencana (Satlak PB) di tingkat Kabupaten / Kota, yang keduanya dibentuk berdasarkan
+    Peraturan Presiden Nomor 83 Tahun 2005.</h5>
+  
+</body>
+
+</html>
---
+++ sejarah.php
@@ -0,0 +1,43 @@
+<html>
+
+<head>
+  <title>PKL BPBD</title>
+  <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="assets/style.css">
+</head>
+
+<body>
+  <div id="container">
+
+    <nav>
+      <div id="logo">
+        <img src="logobpbd4.png" alt="logo bpbd">
+        <span class="textbrand">BPBD</span>
+      </div>
+      <ul>
+        <li><a href="index.php">Beranda</a></li>
+        <li class="dropdown" onmouseover="hover(this);" onmouseout="out(this);"><a href="#">Profil &nbsp;<i class="fa fa-caret-down"></i></a>
+          <div class="dd">
+            <div id="up_arrow"></div>
+            <ul>
+
+              <li><a href="sejarah.php">Sejarah</a></li>
+              <li><a href="visimisi.php">Visi Misi</a></li>
+              <li><a href="struktur.php">Struktur Organisasi</a></li>
+            </ul>
+          </div>
+        </li>
+        <li><a href="peraturan.php">Peraturan</a></li>
+        <li><a href="info.php">Info Kendaraan</a></li>
+        <li><a href="pengaduan.php">Pengaduan</a></li>
+      </ul>
+  </div>
+  </ul>
+  </nav>
+  <img src="bpbd.jpg" alt="Gambar_BPBD" style="width:100%;height:510px;">
+  
+</body>
+
+</html>
